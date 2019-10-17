ebce47dc0b74b5f6d637a571fff438ddc483ddd3
“estephany2828” <ejargoty@unicauca.edu.co>
2019-10-17 00:26:16 -0500

    new  menu principal

---
+++ app/src/main/java/edu/unicauca/patacore/view/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package edu.unicauca.patacore.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.unicauca.patacore.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
